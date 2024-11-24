616d203e16c18d94814cb8cd78ccfc564a5ff8f8
Lutaev Daniil Olegovich <daniillutaev1@gmail.com>
2024-11-24 13:32:52 +0400

    рп

--- gg.m
+++ gg.m
@@ -1,9 +1,9 @@
 # Начало программы должно начинаться только с program var #
 program var
 # Объявление переменных идет после program var #
-$ num1, num2, res
+% num1, num2, res
 begin
-    let num1 = true;
-    num2 = false;
+    let num1 = 1;
+    num2 = 2;
     res = num1 + num2;
 end.
